03e8790fb6effc12a82ae2321905ba8905476044
jerome <jejeproc@gmail.com>
2023-04-22 00:13:57 +0200

    scrollable side data ok

--- src/Components/Planning/SideData.tsx
+++ src/Components/Planning/SideData.tsx
@@ -8,12 +8,33 @@ import {
 import { useAppSelector } from "../../app/hooks";
 import { selectActivities } from "../../features/Redux/activitySlice";
 import draggableStyles from "../Planning/DraggableCardView.module.css";
+import { useRef, useState } from "react";
 export const SIDE_DATA_COL_ID = "sideDataDropId";
 export default function SideData({ scrollYOffset }: { scrollYOffset: number }) {
+  const sideDataRef = useRef<HTMLDivElement>(null);
   const activities = useAppSelector(selectActivities);
+  const [marginTop, setMarginTop] = useState(0);
+
+  const onWheel = (event: React.WheelEvent<HTMLDivElement>) => {
+    setMarginTop((prevState) => {
+      const marginMax = Math.max(
+        0,
+        sideDataRef.current!.clientHeight -
+          sideDataRef.current!.parentElement!.parentElement!.clientHeight
+      );
+      const tempValue = prevState - 150 * Math.sign(event.deltaY);
+      const newMarginTop = Math.max(-marginMax, Math.min(0, tempValue));
+
+      return newMarginTop;
+    });
+  };
   return (
-    <div className={styles.sideDataContainer}>
-      <div className={styles.subContainer}>
+    <div
+      className={styles.sideDataContainer}
+      style={{ marginTop: marginTop }}
+      onWheel={onWheel}
+    >
+      <div ref={sideDataRef} className={styles.subContainer}>
         {activities
           .filter((activity) => !activity.used)
           .map((activity) => (
