e8bae29a1dc11295566cc58073647563ccdb3479
jerome <jejeproc@gmail.com>
2023-04-28 21:14:14 +0200

    fix export

--- src/Components/Planning/Accomodation/AccomodationDropZone.tsx
+++ src/Components/Planning/Accomodation/AccomodationDropZone.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 import styles from "./AccomodationDropZone.module.css";
-export default function () {
+export default function AccomodationDropZone() {
   return <div className={styles.container}> </div>;
 }
